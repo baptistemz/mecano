4703499aefc4ee7c7a137ec1893b4c160322a782
Baptiste <Baptiste@MacBook-Pro-de-user-2.local>
2017-05-18 15:02:41 +0200

    place the react app in the root of the rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  get '/', to: 'react_app#index'
   namespace :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
     get 'authcheck/whoami'
