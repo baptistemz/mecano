e997af466ebdf0bf3de9ba1abd54102dd013fdb3
Baptiste <Baptiste@MacBook-Pro-de-user-2.local>
2017-05-18 16:40:23 +0200

    troubleshooting prod 2

--- client/app/bundle/store/configureStore.js
+++ client/app/bundle/store/configureStore.js
@@ -5,7 +5,7 @@ import reducers from '../reducers';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 export default function configureStore(initialState) {
-  const dev_tools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+  const dev_tools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : {}
   return createStoreWithMiddleware(
     reducers,
     dev_tools
