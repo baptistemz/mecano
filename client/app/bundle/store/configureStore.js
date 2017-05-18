30b7839152378aeb18bab15add6bcbd81807bc13
Baptiste <Baptiste@MacBook-Pro-de-user-2.local>
2017-05-18 16:29:12 +0200

    devtools in development

--- client/app/bundle/store/configureStore.js
+++ client/app/bundle/store/configureStore.js
@@ -5,8 +5,9 @@ import reducers from '../reducers';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 export default function configureStore(initialState) {
+  const dev_tools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : nil
   return createStoreWithMiddleware(
-    reducers, 
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducers,
+    dev_tools
   );
 }
