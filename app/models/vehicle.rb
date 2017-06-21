cd6bd9771d836a506fb42d00fabd3e4ae76f3997
baptistemz <mulliez.baptiste@gmail.com>
2017-06-21 15:40:19 +0200

    vehicle validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :brand, :model, :year
+  validates_presence_of :brand, :model, :year, :user_id
+  validates_uniqueness_of :user_id, scope: [:brand, :model, :year, :trim] 
 end
