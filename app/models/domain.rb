39bbd6d130839bee90c2b5ab7360f956a721c2be
baptistemz <mulliez.baptiste@gmail.com>
2017-06-29 21:29:19 +0200

    add value to domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,6 +2,18 @@ class Domain < ActiveRecord::Base
   extend Enumerize
   belongs_to :mecano_profile
   enumerize :kind, in: [:car_make, :technical_skill]
-  validates_presence_of :kind, :name, :mecano_profile_id
-  validates_uniqueness_of :name, scope: :mecano_profile_id
+  enumerize :value, in: [:wheels,
+                         :brakes,
+                         :timing,
+                         :exhaust,
+                         :gearing_system,
+                         :car_body,
+                         :electricity,
+                         :interior,
+                         :air_conditioning,
+                         :shock_absorbers,
+                         :electronic,
+                         :maintenance], :if => lambda { |o| o.kind = :technical_skill }
+  validates_presence_of :kind, :name, :value, :mecano_profile_id
+  validates_uniqueness_of :name, :value, scope: :mecano_profile_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626165505) do
+ActiveRecord::Schema.define(version: 20170629174724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170626165505) do
     t.string  "name"
     t.string  "kind"
     t.integer "mecano_profile_id"
+    t.string  "value"
     t.index ["mecano_profile_id"], name: "index_domains_on_mecano_profile_id", using: :btree
   end
 
