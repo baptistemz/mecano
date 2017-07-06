1097e55d67ee6a7ebb6bddd9c001218df5dd0a79
baptistemz <mulliez.baptiste@gmail.com>
2017-07-06 12:26:14 +0200

    i18n fallbacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,10 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
+  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
