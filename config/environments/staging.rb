98138baf9948f93fa13ba28fec1dd06e5d8e8d42
baptistemz <mulliez.baptiste@gmail.com>
2017-11-13 10:00:27 +0100

    resolve mailer's host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "mecano.herokuapp.com", :protocol => 'https' }
+  config.action_mailer.default_url_options = { host: ENV['HOST'], :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 
@@ -69,7 +69,7 @@ Rails.application.configure do
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
     :address => 'smtp.sendgrid.net',
-    :domain => 'mecano.herokuapp.com',
+    :domain => ENV['HOST'],
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
