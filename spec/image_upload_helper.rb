545e9c4812a49d115347050c483e2cef03a096c6
baptistemz <mulliez.baptiste@gmail.com>
2017-11-15 18:02:19 +0100

    strict_encode64 in tests

--- spec/image_upload_helper.rb
+++ spec/image_upload_helper.rb
@@ -1,5 +1,5 @@
 def updateImage(status = :ok)
-  profile_picture_params = { profile_picture: "data:image/jpeg;base64,#{Base64.encode64(File.read('spec/factories/images/avatar.jpeg'))}" }
+  profile_picture_params = { profile_picture: "data:image/jpeg;base64,#{Base64.strict_encode64(File.read('spec/factories/images/avatar.jpeg'))}" }
   put api_user_registration_path, params: profile_picture_params, headers: access_tokens
   expect(response).to have_http_status(status)
   payload = parsed_body
